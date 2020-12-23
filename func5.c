104005959ad9120ed4476df3e911d9ea20cca377
Fares84 <fares.sassi2015@gmail.com>
2020-12-23 22:22:06 +0000

    nop

---
+++ func5.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ *nop - function that doesn't anything -.-
+ *@stack: double pointer to a double linked list as stack
+ *@line_number: the number of lines inside the opcode
+ *Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
